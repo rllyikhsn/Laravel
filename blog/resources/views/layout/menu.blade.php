49b85e5c7c0a5f9b2dd2623a5a5d80f2d5573720
Rully Ikhsan <angledark68@gmail.com>
2018-09-29 15:42:07 +0700

    Clear

--- blog/resources/views/layout/menu.blade.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    {{-- menampilkan keseluruhan data --}}
-    <li><a href="{{ route('postCRUD.index') }}">Blog</a></li>
-    <li><a href="{{ route('postCRUD.create') }}">Post Create</a></li>
-</ul>
