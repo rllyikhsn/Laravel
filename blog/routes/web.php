70c481a18f50d9bb9de2653a38ac931b7df65b3f
Rully Ikhsan <angledark68@gmail.com>
2018-10-06 15:03:12 +0700

    Membuat Limit Access Page

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -19,11 +19,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/post', 'PostController@index')->name('post.index');
-Route::get('/post/create', 'PostController@create')->name('post.create');
-Route::post('/post/create', 'PostController@store')->name('post.store');
-Route::get('/post/{post}/edit', 'PostController@edit')->name('post.edit');
-Route::patch('/post/{post}/edit', 'PostController@update')->name('post.update');
-Route::delete('/post/{post}/delete', 'PostController@destroy')->name('post.destroy');
-Route::get('/post/{post}', 'PostController@show')->name('post.show');
-Route::post('/post/{post}/comment', 'PostCommentController@store')->name('post.comment.store');
+Route::middleware('auth')->group(function(){
+    Route::get('/post', 'PostController@index')->name('post.index');
+    Route::get('/post/create', 'PostController@create')->name('post.create');
+    Route::post('/post/create', 'PostController@store')->name('post.store');
+    Route::get('/post/{post}/edit', 'PostController@edit')->name('post.edit');
+    Route::patch('/post/{post}/edit', 'PostController@update')->name('post.update');
+    Route::delete('/post/{post}/delete', 'PostController@destroy')->name('post.destroy');
+    Route::get('/post/{post}', 'PostController@show')->name('post.show');
+    Route::post('/post/{post}/comment', 'PostCommentController@store')->name('post.comment.store');
+});
